aa30ebd7840168dd2efb389e201cce80d176f670
Clint Milner <cmilner@indeed.com>
2020-01-29 23:30:13 -0600

    nailed it!

--- comicpullcalvinespanol/comicpullcalvinespanol.py
+++ comicpullcalvinespanol/comicpullcalvinespanol.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
+from datetime import datetime, timedelta
 from lxml import html
-from urllib.request import urlopen
+from urllib.request import urlopen, urlretrieve
+import wget
 
 
 def main():
-    print("Hello World!")
+    for i in range(30, 1, -1):
+        now = datetime.now() - timedelta(days=i)  # current date and time
+        date_time = now.strftime("%Y/%m/%d")
+        print(date_time)
+
+        tree = html.parse(urlopen("https://www.gocomics.com/calvinandhobbesespanol/" + date_time))
+        for image in tree.xpath("//img/@src"):
+            if image.startswith("https") and not image.find("assets") == -1:
+                print(image)
+                wget.download(image, "./pulledImgs/" + now.strftime("%Y-%m-%d") + ".gif")
 
 
 if __name__ == "__main__":
     main()
 
-print("Clint")
-# html.parse(urlopen("https://www.gocomics.com/calvinandhobbesespanol/2020/01/29"))
