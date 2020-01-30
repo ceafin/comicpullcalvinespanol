844b36103eb093a4fa93c60b075d09c4c9ea8745
Clint Milner <cmilner@indeed.com>
2020-01-29 22:31:59 -0600

    shit actually ran finally...

--- comicpullcalvinespanol/comicpullcalvinespanol.py
+++ comicpullcalvinespanol/comicpullcalvinespanol.py
@@ -1,4 +1,15 @@
-import lxml
+#!/usr/bin/env python
 
-print("Hello.")
+from lxml import html
+from urllib.request import urlopen
 
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
+
+print("Clint")
+# html.parse(urlopen("https://www.gocomics.com/calvinandhobbesespanol/2020/01/29"))
